43737c857f0cd216c49f2e9a62519e282b2833b4
UYasher <shriyash.upadhyay@gmail.com>
2019-09-07 10:50:55 -0400

    Began working on filtering

--- computer-vision/read_from_camera.py
+++ computer-vision/read_from_camera.py
@@ -3,6 +3,14 @@ import cv2
 import numpy as np
 import time
 
+
+def get_angles(img):
+    #hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    #cv2.imshow('hsvimg', hsv_img)
+    red_mask = cv2.inRange(img, (0, 0, 150), (20, 20, 255))
+    cv2.imshow('red_mask', red_mask)
+
+
 URL = "http://192.168.43.1:8080/shot.jpg"
 while True:
     print("just entered loop")
@@ -14,6 +22,7 @@ while True:
     cv2.setWindowProperty("IPWebcam", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow('IPWebcam', img)
     print("just displayed image")
+    get_angles(img)
 
     if cv2.waitKey(10):
         print("waitKey triggered")
