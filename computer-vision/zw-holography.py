5674e62fe75cc946d00ebc397b40b46ea9be0e11
UYasher <shriyash.upadhyay@gmail.com>
2019-09-08 03:11:34 -0400

    Debugged holography

--- computer-vision/zw-holography.py
+++ computer-vision/zw-holography.py
@@ -6,7 +6,7 @@ import os
 # Initialize constants
 width = 640
 height = 360
-blank_frame = np.zeroes(width, height)
+#blank_frame = np.zeros([height, width, 4])
 
 
 def getRandomImage(path):
@@ -16,24 +16,32 @@ def getRandomImage(path):
     """
     files = os.listdir(path)
     index = random.randrange(0, len(files))
-    return cv2.imread(files[index], cv2.IMREAD_UNCHANGED)
+    # print(index)
+    # print(files[index])
+    # print(path+"/"+files[index])
+    return cv2.imread(path + "/" + files[index], cv2.IMREAD_UNCHANGED)
 
 
 class Particle:
     """
     A particle that moves and has an image
     """
-    def __init__(self, img):
-        self.img = img
-        self.v = np.zeros([2])
+
+    def __init__(self, path):
+        self.path = path
+        self.v = np.array([0, 0])
         self.r = np.array([-1, -1])
         self.update()
 
     def update(self):
         self.r += self.v
         if (self.r[0] < 0) or (self.r[0] > width) or (self.r[1] < 0) or (self.r[1] > height):
-            self.r = np.array([random.randint(width), random.randint(height)])
-            self.v = np.array([random.randint(10, 30)], random.randint(10, 100))
+            self.r = np.array([random.randint(0, width), random.randint(0, height)])
+            self.v = np.array([random.randint(-30, 30), random.randint(-30, 30)])
+            self.img = getRandomImage(self.path)
+
+    def __str__(self):
+        return str(self.r) + " moving at " + str(self.v)
 
 
 def zwm(directory, num_particles):
@@ -44,21 +52,40 @@ def zwm(directory, num_particles):
     :return: Simulates and renders a Zach Weinersmith Holograph with the specific images and number of particles
     """
 
+    cv2.imshow("wefwef", getRandomImage(directory))
+    cv2.waitKey(50000)
+
     # Randomly initialize particles
-    particles = [
-        Particle(getRandomImage(directory)) in range(num_particles)
-    ]
+    particles = [Particle(directory) for _ in range(num_particles)]
+
+    cv2.imshow("blank", np.zeros([height, width, 4]))
+    cv2.waitKey(50000)
 
     # Run Holograph
     while True:
-        frame = blank_frame()
+        frame = np.zeros([height, width, 4])
         for p in particles:
+            #print(p)
             # Update position vector
             p.update()
             # Render new frame
-            frame = cv2.addWeighted(frame, 0.4, p.img, 0.1, 0)
+            #print(p.img.shape)
+            # print(frame.shape)
+            frame_to_add = np.zeros([height, width, 4])
+            #print("p.r = " + str(p.r))
+            frame_to_add[p.r[1] + max(0, -p.r[1]):p.r[1] + min(p.img.shape[0], height - p.r[1]),
+            p.r[0] + max(0, -p.r[0]):p.r[0] + min(p.img.shape[1], width - p.r[0])] = \
+                p.img[max(0, -p.r[1]):min(p.img.shape[0],height - p.r[1]), max(0, -p.r[0]):min(p.img.shape[1], width - p.r[0])]
+            #frame = cv2.addWeighted(frame, 0.4, frame_to_add, 0.1, 0)
+            frame = cv2.addWeighted(frame, 0.8, frame_to_add, 0.1, 0)
+        frame = cv2.addWeighted(frame, 0.3, np.zeros([height, width, 4]), 0.7, 0)
+        frame = cv2.addWeighted(frame, 0.3, np.zeros([height, width, 4]), 0.7, 0)
 
         # Display frame in fullscreen
         cv2.namedWindow("Zach Weinersmith Machine", cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty("Zach Weinersmith Machine", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         cv2.imshow("Zach Weinersmith Machine", frame)
+        cv2.waitKey(1)
+
+
+zwm("../images/rand", 20)
